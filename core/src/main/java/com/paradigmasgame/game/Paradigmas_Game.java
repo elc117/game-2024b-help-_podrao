1a9b8da3e586ee4604b369c235cdaee174b6ce35
sosia24 <sosia24@proton.me>
2024-12-09 14:35:05 -0300

    NEW COMMIT

--- core/src/main/java/com/paradigmasgame/game/Paradigmas_Game.java
+++ core/src/main/java/com/paradigmasgame/game/Paradigmas_Game.java
@@ -1,5 +1,6 @@
 package com.paradigmasgame.game;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import com.badlogic.gdx.ApplicationAdapter;
@@ -14,7 +15,6 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.TimeUtils;
 
 public class Paradigmas_Game extends ApplicationAdapter {
     private SpriteBatch batch;
@@ -22,168 +22,280 @@ public class Paradigmas_Game extends ApplicationAdapter {
     private Sprite nave, missile;
     private float posX, posY, xMissile, yMissile;
     private final int velocity = 5;
-    private boolean atack, gameOver;
+    private boolean atack, gameOver, win;
     private long lastEnemyTime;
-    private Array<Rectangle> enemies;
-    private int score;
+    private float enemySpawnInterval = 1.0f; // Intervalo inicial entre spawns (em segundos)
+    private float timeSinceLastSpawn = 0.0f; // Tempo acumulado desde o último spawn
+    private Array<Enemy> enemies;
+    private int score, numQuestions;
     private int power;
-    private int numEnemies;
     private BitmapFont bitmap;
+
+    private int currentQuestionIndex;
+    private ArrayList<Question> questions;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("bg.png");
-        tNave = new Texture("spaceship.png");
+        image = new Texture("planetario.png");
+        tNave = new Texture("podrao.png");
         nave = new Sprite(tNave);
         posX = 0;
         posY = 0;
         xMissile = posX;
         yMissile = posY;
-        lastEnemyTime = 0;
-        numEnemies = 999999999;
-
+        win = false;
         atack = false;
+        numQuestions = 3;
         gameOver = false;
         tMissile = new Texture("missile.png");
         missile = new Sprite(tMissile);
 
-        tEnemy = new Texture("enemy.png");
+        tEnemy = new Texture("haskell.png");
         enemies = new Array<>();
 
         score = 0;
         power = 3;
 
-        // Usando BitmapFont simples, sem a necessidade de FreeTypeFontGenerator
-        bitmap = new BitmapFont(); // Fonte padrão do LibGDX (ou pode carregar um arquivo .fnt)
-        bitmap.setColor(Color.WHITE); // Cor da fonte
+        bitmap = new BitmapFont();
+        bitmap.setColor(Color.WHITE);
+
+        // Criando as perguntas e respostas
+        questions = new ArrayList<>();
+        questions.add(new Question("Qual é a capital do Brasil?", new String[]{"Brasília", "Rio de Janeiro", "São Paulo", "Belo Horizonte"}, 0));
+        questions.add(new Question("Quanto é 2 + 2?", new String[]{"3", "4", "5", "6"}, 1));
+        questions.add(new Question("Qual a cor do céu?", new String[]{"Azul", "Verde", "Amarelo", "Vermelho"}, 0));
+        questions.add(new Question(" ", new String[]{" ", " ", " ", " "}, 0));
+
+        currentQuestionIndex = 0;
+        spawnEnemiesForCurrentQuestion();
     }
 
     @Override
     public void render() {
-        this.moveNave();
-        this.moveMissile();
-        this.moveEnemies();
+        float deltaTime = Gdx.graphics.getDeltaTime();
+    this.moveNave();
+    this.moveMissile();
+    this.moveEnemies();
 
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image,0,0);
-        if(!gameOver){
-            if(atack){
-                batch.draw(missile, xMissile, yMissile);
-            }
-            batch.draw(nave,posX,posY);
-    
-            for(Rectangle enemy: enemies){
-                batch.draw(tEnemy, enemy.x, enemy.y);
-            }
-            bitmap.draw(batch, "Score: "+score, 20, Gdx.graphics.getHeight()-20);
-            bitmap.draw(batch, "Power: "+power, Gdx.graphics.getWidth()-150, Gdx.graphics.getHeight()-20);
-        }else{
-            bitmap.draw(batch, "Score: "+score, 20, Gdx.graphics.getHeight()-20);
-            bitmap.draw(batch, "GAME OVER", Gdx.graphics.getWidth()-150, Gdx.graphics.getHeight()-20);
-            bitmap.draw(batch, "PRESS ENTER", Gdx.graphics.getWidth() / 2-150, Gdx.graphics.getHeight() /2);
-            if(Gdx.input.isKeyPressed(Input.Keys.ENTER)){
-                numEnemies = 999999999;
-                enemies = new Array<>();
-                score = 0;
-                power = 3;
-                posX=0;
-                posY=0;
-                gameOver = false;
-            }
-        }
-        batch.end();
+    timeSinceLastSpawn += deltaTime;
+
+    // Checa se é hora de spawnar um novo inimigo
+    if (timeSinceLastSpawn >= enemySpawnInterval && !gameOver) {
+        spawnEnemy();
+        timeSinceLastSpawn = 0; // Reseta o temporizador
     }
+
+    ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+    batch.begin();
+    batch.draw(image, 0, 0);
+    if (!gameOver) {
+        if (atack) {
+            batch.draw(missile, xMissile, yMissile);
+        }
+        batch.draw(nave, posX, posY);
+
+        for (Enemy enemy : enemies) {
+            batch.draw(tEnemy, enemy.rectangle.x, enemy.rectangle.y);
+
+            // Desenhando o texto das respostas com fonte maior
+            float textX = enemy.rectangle.x + 10;
+            float textY = enemy.rectangle.y + tEnemy.getHeight() + 20;
+
+            bitmap.getData().setScale(2.0f); // Aumenta o tamanho da fonte para as respostas
+
+            bitmap.setColor(Color.BLACK);
+            bitmap.draw(batch, enemy.text, textX + 1, textY - 1); // Sombra preta
+            bitmap.setColor(Color.WHITE);
+            bitmap.draw(batch, enemy.text, textX, textY); // Texto branco
+
+            bitmap.getData().setScale(1.0f); // Volta ao tamanho original após desenhar
+        }
+
+        bitmap.getData().setScale(2.0f);
+        bitmap.draw(batch, "Pergunta: " + questions.get(currentQuestionIndex).question, 20, Gdx.graphics.getHeight() - 20);
+        bitmap.draw(batch, "Score: " + score, 20, Gdx.graphics.getHeight() - 50);
+        bitmap.draw(batch, "Power: " + power, Gdx.graphics.getWidth() - 150, Gdx.graphics.getHeight() - 20);
+    } else {
+        if (win) {
+            bitmap.draw(batch, "YOU WIN", Gdx.graphics.getWidth() / 2 - 100, Gdx.graphics.getHeight() / 2);
+        } else {
+            bitmap.draw(batch, "GAME OVER", Gdx.graphics.getWidth() / 2 - 100, Gdx.graphics.getHeight() / 2);
+        }
+        bitmap.draw(batch, "PRESS ENTER", Gdx.graphics.getWidth() / 2 - 100, Gdx.graphics.getHeight() / 2 - 50);
+        if (Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
+            restartGame();
+        }
+    }
+
+    batch.end();
+}
 
     @Override
     public void dispose() {
         batch.dispose();
         image.dispose();
         tNave.dispose();
-        bitmap.dispose(); // Não se esqueça de liberar recursos de BitmapFont
+        tMissile.dispose();
+        tEnemy.dispose();
+        bitmap.dispose();
     }
 
     private void moveNave() {
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-            if(posX < Gdx.graphics.getWidth()-nave.getWidth()){
-                posX+= velocity;
-            }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && posX < Gdx.graphics.getWidth() - nave.getWidth()) {
+            posX += velocity;
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-            if(posX > 0){
-                posX-= velocity;
-            }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && posX > 0) {
+            posX -= velocity;
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.UP)){
-            if(posY < Gdx.graphics.getHeight()-nave.getHeight()){
-                posY+= velocity;
-            }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP) && posY < Gdx.graphics.getHeight() - nave.getHeight()) {
+            posY += velocity;
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
-            if(posY > 0){
-                posY-= velocity;
-            }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN) && posY > 0) {
+            posY -= velocity;
         }
     }
 
     private void moveMissile() {
-        
-        if(Gdx.input.isKeyPressed(Input.Keys.SPACE) && !atack){
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE) && !atack) {
             atack = true;
-            yMissile = posY  + nave.getHeight() / 2 - 12;
+            yMissile = posY + nave.getHeight() / 2 - 12;
         }
 
-        if(atack){
-            if(xMissile < Gdx.graphics.getWidth()){
-                xMissile+=20;
-            }else{
+        if (atack) {
+            if (xMissile < Gdx.graphics.getWidth()) {
+                xMissile += 20;
+            } else {
                 xMissile = posX + nave.getWidth() / 2;
                 atack = false;
             }
-        }else{
+        } else {
             xMissile = posX + nave.getWidth() / 2;
-            yMissile = posY  + nave.getHeight() / 2 - 12;
+            yMissile = posY + nave.getHeight() / 2 - 12;
         }
     }
 
-    private void spawnEnemys(){
-        Rectangle enemy = new Rectangle(Gdx.graphics.getWidth(), MathUtils.random(0, Gdx.graphics.getHeight() - tEnemy.getHeight()), tEnemy.getWidth(), tEnemy.getHeight());
-        enemies.add(enemy);
-        lastEnemyTime = TimeUtils.nanoTime();
-    }
+    private void spawnEnemiesForCurrentQuestion() {
+        Question currentQuestion = questions.get(currentQuestionIndex);
 
-    private void moveEnemies(){
-        if(TimeUtils.nanoTime() - lastEnemyTime > numEnemies){
-            this.spawnEnemys();
-        }
+    // Seleciona uma resposta aleatória para o inimigo
+    int answerIndex = MathUtils.random(currentQuestion.answers.length - 1);
 
-        for( Iterator<Rectangle> iter = enemies.iterator(); iter.hasNext(); ){
-            Rectangle enemy = iter.next();
-            enemy.x-= 400 * Gdx.graphics.getDeltaTime();
+    float yPosition = MathUtils.random(50, Gdx.graphics.getHeight() - 50);
 
-            if(collide(enemy.x, enemy.y, enemy.width, enemy.height, xMissile, yMissile, missile.getWidth(), missile.getHeight()) && atack ){
-               ++score;
-               if(score % 5 == 0 && numEnemies > 150000000){
-                numEnemies -= 70000000;
-               }
-                atack = false;
-                iter.remove();
-            }else if(collide(enemy.x, enemy.y, enemy.width, enemy.height, posX, posY, nave.getWidth(), nave.getHeight())&& !gameOver){
-                --power;
-                if(power <=0){
+    Rectangle enemyRectangle = new Rectangle(
+            Gdx.graphics.getWidth(),
+            yPosition - (tEnemy.getHeight() / 2)+20,
+            tEnemy.getWidth(),
+            tEnemy.getHeight());
+    
+    enemies.add(new Enemy(enemyRectangle, currentQuestion.answers[answerIndex], answerIndex == currentQuestion.correctIndex));
+}
+
+private void moveEnemies() {
+    boolean allEnemiesOut = true;
+    boolean correctHit = false; // Sinalizador para verificar acerto correto
+
+    for (Iterator<Enemy> iter = enemies.iterator(); iter.hasNext(); ) {
+        Enemy enemy = iter.next();
+        enemy.rectangle.x -= 400 * Gdx.graphics.getDeltaTime();
+
+        // Colisão com o míssil
+        if (collide(enemy.rectangle.x, enemy.rectangle.y, enemy.rectangle.width, enemy.rectangle.height, xMissile, yMissile, missile.getWidth(), missile.getHeight()) && atack) {
+            if (enemy.isCorrect) {
+                score++;
+                correctHit = true; // Marca que a resposta correta foi atingida
+            } else {
+                power--;
+                if (power <= 0) {
                     gameOver = true;
                 }
-                iter.remove();
             }
+            atack = false;
+            iter.remove();
+        } else if (enemy.rectangle.x + tEnemy.getWidth() < 0) {
+            iter.remove();
+        }
 
-            if(enemy.x +tEnemy.getWidth() < 0){
-                iter.remove();
-            }
+        if (enemy.rectangle.x + tEnemy.getWidth() > 0) {
+            allEnemiesOut = false;
         }
     }
 
-    private boolean collide(float x1, float y1, float w1, float h1, float x2, float y2, float w2, float h2){
+    // Se a resposta correta foi atingida, avança para a próxima pergunta
+    if (correctHit) {
+        currentQuestionIndex = currentQuestionIndex + 1;
+        if (currentQuestionIndex >= numQuestions) {
+            enemies.clear();
+            win = true; // Define vitória quando todas as perguntas forem respondidas
+            gameOver = true; // Termina o jogo
+        }
+        enemies.clear(); // Agora pode limpar a lista sem problemas
+        spawnEnemiesForCurrentQuestion();
+    }
+
+    // Caso todos os inimigos saiam da tela, continua spawnando
+    if (allEnemiesOut && !gameOver) {
+        spawnEnemy();
+    }
+}
+
+
+    private boolean collide(float x1, float y1, float w1, float h1, float x2, float y2, float w2, float h2) {
         return x1 + w1 > x2 && x1 < x2 + w2 && y1 + h1 > y2 && y1 < y2 + h2;
+    }
+
+    private void restartGame() {
+        enemies.clear();
+        score = 0;
+        power = 3;
+        posX = 0;
+        posY = 0;
+        gameOver = false;
+        win = false;
+        currentQuestionIndex = 0;
+        spawnEnemiesForCurrentQuestion();
+    }
+
+    private void spawnEnemy() {
+        Question currentQuestion = questions.get(currentQuestionIndex);
+    
+        // Seleciona uma resposta aleatória para o inimigo
+        int answerIndex = MathUtils.random(currentQuestion.answers.length - 1);
+    
+        float yPosition = MathUtils.random(50, Gdx.graphics.getHeight() - 50);
+    
+        Rectangle enemyRectangle = new Rectangle(
+                Gdx.graphics.getWidth(),
+                yPosition - tEnemy.getHeight() / 2,
+                tEnemy.getWidth(),
+                tEnemy.getHeight());
+        
+        enemies.add(new Enemy(enemyRectangle, currentQuestion.answers[answerIndex], answerIndex == currentQuestion.correctIndex));
+    }
+    
+
+    private static class Enemy {
+        Rectangle rectangle;
+        String text;
+        boolean isCorrect;
+
+        Enemy(Rectangle rectangle, String text, boolean isCorrect) {
+            this.rectangle = rectangle;
+            this.text = text;
+            this.isCorrect = isCorrect;
+        }
+    }
+
+    private static class Question {
+        String question;
+        String[] answers;
+        int correctIndex;
+
+        Question(String question, String[] answers, int correctIndex) {
+            this.question = question;
+            this.answers = answers;
+            this.correctIndex = correctIndex;
+        }
     }
 }
--- html/src/main/java/com/paradigmasgame/game/gwt/GwtLauncher.java
+++ html/src/main/java/com/paradigmasgame/game/gwt/GwtLauncher.java
@@ -9,14 +9,8 @@ import com.paradigmasgame.game.Paradigmas_Game;
 public class GwtLauncher extends GwtApplication {
         @Override
         public GwtApplicationConfiguration getConfig () {
-            // Resizable application, uses available space in browser with no padding:
-            GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(true);
-            cfg.padVertical = 0;
-            cfg.padHorizontal = 0;
-            return cfg;
-            // If you want a fixed size application, comment out the above resizable section,
-            // and uncomment below:
-            //return new GwtApplicationConfiguration(640, 480);
+            // Definindo um tamanho fixo para o jogo
+            return new GwtApplicationConfiguration(1280, 720);  // Substitua pelos valores desejados
         }
 
         @Override
